15fce9b97702be9f9fb2b456c41eef78c6bb762b
vicky <vickykumarsunilja@gmail.com>
2024-06-17 08:22:15 +0530

    created models

---
+++ wikichat/server/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="server_owner")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="server_category")
+    description = models.CharField(max_length=300, null=True)
+    member= models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="channel_owner")
+    topic = models.CharField(max_length=100)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE,related_name='channel_server')
+
+    def __str__(self):
+        return self.name
+    
